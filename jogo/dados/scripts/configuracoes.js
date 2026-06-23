47f3e3c6dd9ed1d2a449eaeccb7cd9bba823f8eb
euuu1234 <loginkaralho@gmail.com>
2026-06-23 16:07:01 -0300

    adicionar pasta dados

---
+++ jogo/dados/scripts/configuracoes.js
@@ -0,0 +1,9 @@
+//-------------------------------------------------------------------------tela------------------------------------------------------//
+
+const configuracoes = {};
+configuracoes["displayWidth"] = screen.width;
+configuracoes["displayHeight"] = screen.height;
+
+//--------------------------------------------------------------------exportação------------------------------------------------------//
+
+export default configuracoes;
